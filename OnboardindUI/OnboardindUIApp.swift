667fb304ed64e64151703e42b8891053b4af24d4
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-05 19:27:38 +0200

    Initial Commit

---
+++ OnboardindUI/OnboardindUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardindUIApp.swift
+//  OnboardindUI
+//
+//  Created by Ndayisenga Jean Claude on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnboardindUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
